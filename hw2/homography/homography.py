51631f7d70dd6242a5301ffe810384c1a85b1d7f
SupradeepDanturti <suppualn@gmail.com>
2024-03-14 10:47:07 -0400

    homo

--- hw2/homography/homography.py
+++ hw2/homography/homography.py
@@ -1,54 +1,51 @@
-import cv2
-import numpy as np
-
+from skimage.feature import SIFT, match_descriptors
+from skimage.color import rgb2gray
 def matchPics(I1, I2):
-    """https://docs.opencv.org/4.x/da/df5/tutorial_py_sift_intro.html"""
     # Given two images I1 and I2, perform SIFT matching to find candidate match pairs
 
     ### YOUR CODE HERE
     ### You can use skimage or OpenCV to perform SIFT matching
-    I1 = np.uint8(I1 * 255)
-    I2 = np.uint8(I2 * 255)
 
-    sift = cv2.SIFT_create()
+    I2 = rgb2gray(I2)
+    descriptor_extractor = SIFT()
 
-    keypoints_1, descriptors_1 = sift.detectAndCompute(I1, None)
-    keypoints_2, descriptors_2 = sift.detectAndCompute(I2, None)
+    descriptor_extractor.detect_and_extract(I1)
+    locs1 = descriptor_extractor.keypoints
+    descriptors1 = descriptor_extractor.descriptors
 
-    bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
+    descriptor_extractor.detect_and_extract(I2)
+    locs2 = descriptor_extractor.keypoints
+    descriptors2 = descriptor_extractor.descriptors
 
-    matches = bf.match(descriptors_1, descriptors_2)
-    matches = sorted(matches, key=lambda x: x.distance)
-
-    locs1 = np.array([keypoints_1[m.queryIdx].pt for m in matches])
-    locs2 = np.array([keypoints_2[m.trainIdx].pt for m in matches])
-
-    print(locs1)
+    matches = match_descriptors(descriptors1, descriptors2, max_ratio=0.6,
+                                cross_check=True)
+    # print(matches)
     ### END YOUR CODE
     return matches, locs1, locs2
 
-
 def computeH_ransac(matches, locs1, locs2):
-    # Compute the best fitting homography using RANSAC given a list of matching pairs
 
+    # Compute the best fitting homography using RANSAC given a list of matching pairs
+    
     ### YOUR CODE HERE
     ### You should implement this function using Numpy only
-
+    
     ### END YOUR CODE
 
     return bestH, inliers
 
-
 def compositeH(H, template, img):
+
     # Create a compositie image after warping the template image on top
     # of the image using homography
 
-    # Create mask of same size as template
 
-    # Warp mask by appropriate homography
+    #Create mask of same size as template
 
-    # Warp template by appropriate homography
+    #Warp mask by appropriate homography
 
-    # Use mask to combine the warped template and the image
+    #Warp template by appropriate homography
 
+    #Use mask to combine the warped template and the image
+    
     return composite_img
--- hw2/homography/run.py
+++ hw2/homography/run.py
@@ -17,21 +17,21 @@ matches, locs1, locs2 = matchPics(cv_cover, cv_desk)
 # visualize raw matching result
 visualize_match(cv_cover, cv_desk_gray, locs1, locs2, matches)
 
-# # resize hp_cover to have the same size of cv_cover
-# hp_cover_resize = resize(hp_cover, cv_cover.shape, anti_aliasing = True)
-#
-# # use RANSAC to estimate homograhy and find inlier matches
-# bestH, inliers = computeH_ransac(matches, locs1, locs2)
-#
-# # visualize matching result after RANSAC
-# visualize_match(cv_cover, cv_desk_gray, locs1, locs2, matches[inliers, :])
-#
-# # visualize the bounding box in target image
-# visualize_box(cv_cover, cv_desk, bestH)
-#
-# # create final composite image
-# composite_img = compositeH(bestH, hp_cover_resize, cv_desk)
-#
-# # show the final composite image
-# plt.imshow(composite_img)
-# plt.show()
+# resize hp_cover to have the same size of cv_cover
+hp_cover_resize = resize(hp_cover, cv_cover.shape, anti_aliasing = True)
+
+# use RANSAC to estimate homograhy and find inlier matches
+bestH, inliers = computeH_ransac(matches, locs1, locs2)
+
+# visualize matching result after RANSAC
+visualize_match(cv_cover, cv_desk_gray, locs1, locs2, matches[inliers, :])
+
+# visualize the bounding box in target image
+visualize_box(cv_cover, cv_desk, bestH)
+
+# create final composite image
+composite_img = compositeH(bestH, hp_cover_resize, cv_desk)
+
+# show the final composite image
+plt.imshow(composite_img)
+plt.show()
